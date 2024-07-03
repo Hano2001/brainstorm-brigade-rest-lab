4cac88d3cfef002d62c1a30240e96d06e71646ef
Oriana Mendez <egleemendez@gmail.com>
2024-07-03 13:16:04 +0200

    created status endpoint
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,9 @@ import express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/status", (req, res) => {
+  res.statusCode = 200;
+  res.send();
 });
 
 app.listen(port, () => {
