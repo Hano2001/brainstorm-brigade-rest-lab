f26c14bd1d2ebe02e9bad2bc0c552bfbad7eab8e
lukas-laas <lukas.s.laas@gmail.com>
2024-07-04 13:25:44 +0200

    zod validation post
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,31 +1,38 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import z from "zod";
+
 const app = express();
 const port = 3000;
-type Message = {
-  id: string;
-  message: string;
-};
+
+const Message = z.object({
+  id: z.string(),
+  message: z.string(),
+});
+
+type Message = z.infer<typeof Message>;
+
 let messages: Message[] = [];
 
 app.use(express.json());
 
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.get("/status", (req, res) => {
   res.statusCode = 200;
   res.send();
 });
 
-app.get('/greetings', (req, res) => {
+app.get("/greetings", (req, res) => {
   res.statusCode = 200;
   res.json(messages);
 });
 
-app.post('/greetings', (req, res) => {
+app.post("/greetings", (req, res) => {
   console.table(req.body);
   const { message } = req.body;
   const messageObject = { id: messages.length.toString(), message: message };
+  Message.parse(messageObject);
   messages.push(messageObject);
   res.statusCode = 201;
   res.json(messageObject);
